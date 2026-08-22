3a13a056d805f28c7c0ec0070f5468fe0810d9fd
sanjeevm4315-lab <sanjeev.m4315@gmail.com>
2026-08-22 21:48:46 +0530

    Add files via upload

---
+++ chapter-5/04_quick_quiz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    // Use the library function to calculate the area of a square with side a.
+    //  Solve this in c language
+
+    float a =5;
+    printf("The area of the square with side %f is %f\n", a, pow(a, 2));
+    return 0;
+}
